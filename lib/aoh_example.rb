c38f6c3240ef7ab44212be15de0e93627c097e64
Aaron Muesing <muesing.music@gmail.com>
2021-01-02 15:37:40 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,11 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
   [
   { name:  "Don Gately", occupation: "Live-in Staff" },
-  { name:  "Joelle van Dyne", occupation: "Radio Personality" } ]
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
+  { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  ]
 end
 
 def literal_aoh
