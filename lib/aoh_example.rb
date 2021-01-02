60d9411dc120620f74c9b9267a6123d9fe750394
Aaron Muesing <muesing.music@gmail.com>
2021-01-02 16:02:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,7 +21,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  characters = [
+  aoh = [
   { name:  "Don Gately", occupation: "Live-in Staff" },
   { name:  "Joelle van Dyne", occupation: "Radio Personality" },
   { name:  "Pat Monteseian", occupation: "Staff" },
@@ -31,6 +31,7 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
